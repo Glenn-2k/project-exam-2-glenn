69a2a2eb3dc417c64da4c47cb7b6211993adcdc2
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-01-16 13:52:27 +0100

    add RouteNotFound component for handling 404 errors in routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import Layout from "./components/Layout";
 
+function RouteNotFound() {
+  return <div>Page not found</div>;
+}
+
 function App() {
   return (
     <div className="h-screen">
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route path="*" element={<RouteNotFound />} />
       </Routes>
     </div>
   );
