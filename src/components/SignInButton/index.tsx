2df45c916315ef26d37781e86f562facf63fedf5
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-01-20 12:17:04 +0100

    add SignInButton component and integrate it into the Header

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,10 @@
+export const SignInButton = () => (
+  <button
+    type="button"
+    className="flex items-center m-4 h-12 text-white bg-primary-500 rounded-lg"
+  >
+    Sign in
+  </button>
+);
+
+export default SignInButton;
