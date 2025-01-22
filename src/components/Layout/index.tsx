fecdaa58e2514e0e64f77e3029e95fe1d895e2bf
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-01-22 11:35:27 +0100

    fix: add background color to main layout for improved visibility

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,7 +6,7 @@ function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow pt-16">
+      <main className="bg-gray-200 flex-grow pt-16">
         <Outlet />
       </main>
       {/* <Footer /> */}
