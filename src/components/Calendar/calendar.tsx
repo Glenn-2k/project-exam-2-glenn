06ab6cccf20f52a6a74d950d981b8fe19e63d5ec
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-01-23 13:18:05 +0100

    feat: enhance Calendar component with date range selection functionality

--- src/components/Calendar/calendar.tsx
+++ src/components/Calendar/calendar.tsx
@@ -5,8 +5,29 @@ import "react-datepicker/dist/react-datepicker.css";
 
 const Calendar = () => {
   const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const onChange = (dates: [Date | null, Date | null]) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+  function addMonths(date: Date, months: number): Date {
+    const result = new Date(date);
+    result.setMonth(result.getMonth() + months);
+    return result;
+  }
+
   return (
-    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    <DatePicker
+      selected={startDate}
+      onChange={onChange}
+      startDate={startDate}
+      endDate={endDate}
+      inline
+      minDate={new Date()}
+      maxDate={addMonths(new Date(), 5)}
+      selectsRange
+    />
   );
 };
 
