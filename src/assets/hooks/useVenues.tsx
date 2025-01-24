cb6e440d88f7256b1c2ff39644a791b8060d8ede
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-01-24 12:46:52 +0100

    feat: enhance useVenues hook with pagination and add useSearchVenues hook for venue searching

--- src/assets/hooks/useVenues.tsx
+++ src/assets/hooks/useVenues.tsx
@@ -3,15 +3,21 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchFn } from "../../utilities/http";
 import * as VenueTypes from "../../Types/venues.t";
 
-export const useVenues = (sort = "created", sortOrder = "desc") => {
+export const useVenues = (
+  page = 1,
+  limit = 100,
+  sort = "created",
+  sortOrder = "desc"
+) => {
   return useQuery<VenueTypes.VenueResponse>({
-    queryKey: [venuesUrl, "venues", { sort, sortOrder }],
+    queryKey: [venuesUrl, "venues", { sort, sortOrder, page, limit }],
     queryFn: () =>
       fetchFn({
         queryKey: [
-          `${venuesUrl}?sort=${sort}&sortOrder=${sortOrder}`,
+          `${venuesUrl}?limit=${limit}&page=${page}&sort=${sort}&sortOrder=${sortOrder}`,
           "venues",
         ],
       }),
+    staleTime: 5000,
   });
 };
