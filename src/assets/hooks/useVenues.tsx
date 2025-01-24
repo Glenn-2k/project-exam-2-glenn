982f23e9f35b615024df3dc014a29303dc2f2cbf
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-01-24 11:07:21 +0100

    feat: enhance useVenues hook to support sorting options

--- src/assets/hooks/useVenues.tsx
+++ src/assets/hooks/useVenues.tsx
@@ -3,9 +3,15 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchFn } from "../../utilities/http";
 import * as VenueTypes from "../../Types/venues.t";
 
-export const useVenues = () => {
+export const useVenues = (sort = "created", sortOrder = "desc") => {
   return useQuery<VenueTypes.VenueResponse>({
-    queryKey: [venuesUrl, "venues"],
-    queryFn: () => fetchFn({ queryKey: [venuesUrl, "venues"] }),
+    queryKey: [venuesUrl, "venues", { sort, sortOrder }],
+    queryFn: () =>
+      fetchFn({
+        queryKey: [
+          `${venuesUrl}?sort=${sort}&sortOrder=${sortOrder}`,
+          "venues",
+        ],
+      }),
   });
 };
