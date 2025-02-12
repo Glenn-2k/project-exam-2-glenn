43bc174d1fdd2f7b97ac5f66c0fa046a4274349f
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-02-12 13:06:06 +0100

    feat: update Logo component to reload page on home click or navigate to home

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,25 @@
 import logoWhite from "../assets/logoWhite.png";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Logo = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleClick = () => {
+    if (location.pathname === "/") {
+      window.location.reload();
+    } else {
+      navigate("/");
+    }
+  };
+
   return (
     <div>
       <img
         src={logoWhite}
         alt="Logo"
         className="h-12 w-auto m-4 pl-6 cursor-pointer"
-        onClick={() => navigate("/")}
+        onClick={handleClick}
       />
     </div>
   );
