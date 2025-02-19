41cccc685ec58b44bfd7384a7f0238c0692ef0b9
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2025-02-19 12:29:10 +0100

    feat: add request interception and validation for login functionality in Cypress tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,15 @@
 describe("Login", () => {
   it("should login with valid credentials", () => {
     cy.visit("/login");
+    cy.intercept("POST", "/auth/login").as("loginRequest");
     cy.get('input[name="email"]').type(Cypress.env("cypressEmail"));
     cy.get('input[name="password"]').type(Cypress.env("cypressPassword"));
     cy.get('[data-testid="login-button"]').click();
+
+    cy.wait("@loginRequest").then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+    });
+
     cy.url().should("include", "/");
   });
 
